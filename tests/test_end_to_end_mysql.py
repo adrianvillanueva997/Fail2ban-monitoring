bfd95bcb9196875328b46cbf46d24bc0dd791ec6
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2025-04-29 15:56:30 +0900

    try another approach

--- tests/test_end_to_end_mysql.py
+++ tests/test_end_to_end_mysql.py
@@ -5,20 +5,19 @@ import aiohttp
 import anyio
 import pytest
 from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 from testcontainers.mysql import MySqlContainer
 
 from fail2banmonitoring.db.config import SqlConnectorConfig, SqlEngine
 from fail2banmonitoring.fail2ban.log_parser import Fail2BanLogParser
-from fail2banmonitoring.models.base import _Base
 from fail2banmonitoring.models.ip import IpModel
 from fail2banmonitoring.services.ip import IPMetadata
 from fail2banmonitoring.utils.environment_variables import EnvironmentVariables
 
 
-def set_env_vars(mysql, tmp_path) -> None:
+def set_env_vars(mysql: MySqlContainer, tmp_path: Path) -> None:
     os.environ["DRIVER"] = "mysql+aiomysql"
-    os.environ["HOST"] = "127.0.0.1"
+    os.environ["HOST"] = mysql.get_container_host_ip()
     os.environ["PORT"] = str(mysql.port)
     os.environ["USERNAME"] = mysql.username
     os.environ["PASSWORD"] = mysql.password
@@ -27,7 +26,7 @@ def set_env_vars(mysql, tmp_path) -> None:
     os.environ["EXPORT_IP_PATH"] = str(tmp_path / "banned.txt")
 
 
-def prepare_fake_log(log_path):
+def prepare_fake_log(log_path: str):
     async def _write() -> None:
         async with await anyio.open_file(log_path, "w") as f:
             await f.write(
@@ -49,12 +48,6 @@ async def test_end_to_end_mysql(tmp_path) -> None:
         log_path = os.environ["LOG_PATH"]
         await prepare_fake_log(log_path)
 
-        # Create tables
-        db_url = mysql.get_connection_url().replace("mysql://", "mysql+aiomysql://")
-        engine = create_async_engine(db_url, echo=True)
-        async with engine.begin() as conn:
-            await conn.run_sync(_Base.metadata.create_all)
-
         environment_variables = EnvironmentVariables()
         if environment_variables.log_path is None:
             msg = "LOG_PATH environment variable is not set"
@@ -63,13 +56,7 @@ async def test_end_to_end_mysql(tmp_path) -> None:
             log_path=environment_variables.log_path,
             output_file=environment_variables.export_ip_path,
         )
-
-        ips = parser.read_logs()
-        assert "8.8.8.8" in ips  # noqa: S101
-
-        async with aiohttp.ClientSession() as session:
-            enriched = await IPMetadata.get_ips_metadata_batch(list(ips), session)
-
+        # Create tables
         if environment_variables.port is None:
             msg = "PORT environment variable is not set"
             raise ValueError(msg)
@@ -83,10 +70,17 @@ async def test_end_to_end_mysql(tmp_path) -> None:
         )
 
         sql_engine = SqlEngine(url_config=sql_config)
+        await IpModel.create_table(sql_engine)
+
+        ips = parser.read_logs()
+        assert "8.8.8.8" in ips  # noqa: S101
+
+        async with aiohttp.ClientSession() as session:
+            enriched = await IPMetadata.get_ips_metadata_batch(list(ips), session)
+
         await IpModel.insert(enriched, sql_engine)
 
-        # Check DB for inserted IP - use column name ip_address
-        async with AsyncSession(engine) as session:
+        async with AsyncSession(sql_engine.engine.engine) as session:
             result = await session.execute(
                 text("SELECT ip_address FROM ip WHERE ip_address = '8.8.8.8'"),
             )
